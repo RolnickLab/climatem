2a36b66ab73659118eb30dd38bcfa3a489b9badc
Sebastian Hickman <seb.hickman@gmail.com>
2025-02-03 09:48:07 -0500

    adding sbatch script and config so ready to go

--- scripts/run_cdsd_predictions_testing_sparsconst_nonlinear_ensembles_hilatent_multigpu_ts.sh
+++ scripts/run_cdsd_predictions_testing_sparsconst_nonlinear_ensembles_hilatent_multigpu_ts.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+#SBATCH --partition=long                                                 # Ask for long job
 #SBATCH --gpus-per-task=1                                                # Ask for 1 GPU
 #SBATCH --cpus-per-task=16                                               # Ask for 2 CPUs
 #SBATCH --ntasks-per-node=1                                              # Ask for 2 CPUs
 #SBATCH --nodes=1                                                        # Ask for 2 CPUs
 #SBATCH --mem=80G                                                       # Ask for 10 GB of RAM
 #SBATCH --time=16:00:00                                                  # The job will run for 10 hours
-#SBATCH -o /scratch/slurm-%j.out                                        # Write the log on scratch
+#SBATCH -o /network/scratch/s/sebastian.hickman/slurm_logs/slurm-%j.out  # Write the log on scratch
 
 # 0. Clear the environment
 module purge
@@ -15,7 +16,7 @@ module purge
 module --quiet load python/3.10
 
 # 2. Load your environment
-source $HOME/work/climatem/env_emu_poetry/bin/activate
+source $HOME/work/new_climatem/climatem/env_emulator_poetry/bin/activate
 
 
 # Get a unique port for this job based on the job ID
@@ -34,5 +35,5 @@ accelerate launch \
     --num_processes=1 \
     --num_machines=1 \
     --gpu_ids='all' \
-    $HOME/work/climatem/scripts/main_explore_predictions_ar_sparsity_constraint_explore_ensembles_multigpu_accelerate_no_crps_code_review.py --no-gt --tau 5 --gpu --d-z 90 --d-x 6250 --config-exp-path $HOME/work/climatem/scripts/configs/climate_predictions_picontrol_icosa_nonlinear_ensembles_hilatent_all_icosa_picontrol.json --exp-path $HOME/scratch/results/climatem_spectral_no_crps_no_detach_github_close/ --config-path $HOME/work/climatem/scripts/params/default_params_testing_sparsconst_sbatch_nl_ensembles_relax_single_hilatent.json --lr 0.001 --reg-coeff 0.31 --sparsity-upper-threshold 0.5
+    $HOME/work/new_climatem/climatem/scripts/main_explore_predictions_ar_sparsity_constraint_explore_ensembles_multigpu_accelerate.py --no-gt --tau 5 --gpu --d-z 90 --d-x 6250 --config-exp-path $HOME/work/new_climatem/climatem/scripts/configs/climate_predictions_picontrol_icosa_nonlinear_ensembles_hilatent_all_icosa.json --exp-path $HOME/scratch/results/new_climatem_spectral/ --config-path $HOME/work/new_climatem/climatem/scripts/params/default_params_testing_sparsconst_sbatch_nl_ensembles_relax_single_hilatent_smash_spectra.json --lr 0.001 --reg-coeff 0.2 --sparsity-upper-threshold 0.5
 
