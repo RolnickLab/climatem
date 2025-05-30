739e9e0c9b3a4c70fd30e90b368171a8efcfa93f
julien.boussard <jb4365@columbia.edu>
2025-05-30 17:34:47 -0400

    no gev

--- scripts/main_picabu.py
+++ scripts/main_picabu.py
@@ -141,10 +141,11 @@ def main(
         reduce_encoding_pos_dim=model_params.reduce_encoding_pos_dim,
         coeff_kl=optim_params.coeff_kl,
         d=d,
+        #Here, everything hardcoded to gaussian because GEV leads to Nan... TBD
         distr_z0="gaussian",
         distr_encoder="gaussian",
         distr_transition="gaussian",
-        distr_decoder="gev",
+        distr_decoder="gaussian",
         d_x=experiment_params.d_x,
         d_z=experiment_params.d_z,
         tau=experiment_params.tau,
