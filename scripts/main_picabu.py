2f49ce49a6f83106b819a399dddb18d7169e854b
ChristinaLast <christina.last@outlook.com>
2025-06-01 21:04:57 -0400

    adjusting name to include source and var

--- scripts/main_picabu.py
+++ scripts/main_picabu.py
@@ -168,11 +168,8 @@ def main(
     # Make folder to save run results
     exp_path = Path(experiment_params.exp_path)
     os.makedirs(exp_path, exist_ok=True)
-    data_var_ids_str = (
-        str(data_params.in_var_ids)[1:-1]
-        .translate({ord("'"): None})
-        .translate({ord(","): None})
-        .translate({ord(" "): None})
+    data_var_ids_str = "_".join(
+        f"{source}_{'_'.join(vars)}" for source, vars in data_params.in_var_ids.items()
     )
     name = f"var_{data_var_ids_str}_scen_{data_params.train_scenarios[0]}_nlinmix_{model_params.nonlinear_mixing}_nlindyn_{model_params.nonlinear_dynamics}_tau_{experiment_params.tau}_z_{experiment_params.d_z}_futt_{experiment_params.future_timesteps}_ldec_{optim_params.loss_decay_future_timesteps}_lr_{train_params.lr}_bs_{data_params.batch_size}_ormuin_{optim_params.ortho_mu_init}_spmuin_{optim_params.sparsity_mu_init}_spth_{optim_params.sparsity_upper_threshold}_nens_{data_params.num_ensembles}_inst_{model_params.instantaneous}_crpscoef_{optim_params.crps_coeff}_sspcoef_{optim_params.spectral_coeff}_tspcoef_{optim_params.temporal_spectral_coeff}_fracnhiwn_{optim_params.fraction_highest_wavenumbers}_nummix_{model_params.num_hidden_mixing}_numhid_{model_params.num_hidden}_embdim_{model_params.position_embedding_dim}"
     exp_path = exp_path / name
