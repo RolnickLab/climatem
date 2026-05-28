f75110ebf30ae3dd666513db4485076eb42a0c32
julienboussard <julien.boussard@mila.quebec>
2026-05-28 12:27:01 -0400

    Implement remap_healpix_to_reg function
    Added a function to remap data from a HEALPix grid to a regular lat/lon grid using linear and nearest-neighbor interpolation.

--- climatem/data_loader/healpix_remapping.py
+++ climatem/data_loader/healpix_remapping.py
@@ -1,8 +1,7 @@
 import healpy as hp
 import numpy as np
-from scipy.interpolate import RegularGridInterpolator
+from scipy.interpolate import RegularGridInterpolator, LinearNDInterpolator, NearestNDInterpolator
 from tqdm import trange
-
 
 def remap_reg_to_healpix(
     arr,
@@ -43,3 +42,69 @@ def remap_reg_to_healpix(
     remapped_data = remapped_data.reshape(final_shape)
 
     return remapped_data, latitudes_new, longitudes_new
+
+def remap_healpix_to_reg(
+    arr,
+    coords_lon,
+    coords_lat,
+):
+    """
+    Remap data from a HEALPix grid to a regular lat/lon grid.
+
+    Parameters
+    ----------
+    arr : np.ndarray
+        Input array with HEALPix data, shape (..., npix).
+        Any number of leading dimensions is supported.
+    coords_lon : np.ndarray
+        1D array of target longitudes in degrees [0, 360).
+    coords_lat : np.ndarray
+        1D array of target latitudes in degrees [-90, 90].
+
+    Returns
+    -------
+    remapped_data : np.ndarray
+        Remapped array of shape (..., n_lat, n_lon).
+    """
+    npix = arr.shape[-1]
+    NSIDE = hp.npix2nside(npix)
+
+    # Get the lat/lon of every HEALPix pixel
+    theta, phi = hp.pix2ang(NSIDE, np.arange(npix))
+    lat_hp = np.degrees(np.pi / 2 - theta)   # [-90, 90]
+    lon_hp = np.degrees(phi)                  # [0, 360)
+
+    # Build the regular grid query points
+    lon_grid, lat_grid = np.meshgrid(coords_lon, coords_lat)  # (n_lat, n_lon)
+    grid_points = np.column_stack([lat_grid.ravel(), lon_grid.ravel()])
+
+    # Source points for the interpolator
+    src_points = np.column_stack([lat_hp, lon_hp])
+
+    original_shape = arr.shape
+    arr_flat = arr.reshape(-1, npix)          # (N, npix)
+
+    n_lat = len(coords_lat)
+    n_lon = len(coords_lon)
+    remapped_flat = np.zeros((arr_flat.shape[0], n_lat * n_lon))
+
+    for k in trange(arr_flat.shape[0]):
+        values = arr_flat[k]                  # (npix,)
+
+        # Primary: linear interpolation from scattered HEALPix points
+        interp_linear = LinearNDInterpolator(src_points, values)
+        result = interp_linear(grid_points)
+
+        # Fallback: nearest-neighbour for any NaNs near the poles / hull edges
+        nan_mask = np.isnan(result)
+        if nan_mask.any():
+            interp_nearest = NearestNDInterpolator(src_points, values)
+            result[nan_mask] = interp_nearest(grid_points[nan_mask])
+
+        remapped_flat[k] = result
+
+    # Restore leading dimensions and append (n_lat, n_lon)
+    new_shape = original_shape[:-1] + (n_lat, n_lon)
+    remapped_data = remapped_flat.reshape(new_shape)
+
+    return remapped_data
